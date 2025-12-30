096a18d2101b2ba659215a0797fb30781374ab15
richienagle <richienagle@gmail.com>
2025-12-30 10:39:53 -0800

    Initial commit with .gitignore

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Rich Nagle on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreakerTests/CodeBreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerTests.swift
+//  CodeBreakerTests
+//
+//  Created by Rich Nagle on 12/30/25.
+//
+
+import Testing
+@testable import CodeBreaker
+
+struct CodeBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
