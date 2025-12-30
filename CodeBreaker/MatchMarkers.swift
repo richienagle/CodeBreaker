efe7655ef17399deee5c08bda77de6888dea01f6
richienagle <richienagle@gmail.com>
2025-12-30 10:56:25 -0800

    initial commit of code changes up to Lecture 2

--- CodeBreaker/MatchMarkers.swift
+++ CodeBreaker/MatchMarkers.swift
@@ -5,4 +5,39 @@
 //  Created by Rich Nagle on 12/30/25.
 //
 
-import Foundation
+import SwiftUI
+
+enum Match {
+    case nomatch
+    case exact
+    case inexact
+}
+
+struct MatchMarkers: View {
+    var matches: [Match]
+    
+    var body : some View {
+        HStack {
+            VStack {
+                matchMarker(peg: 0)
+                matchMarker(peg: 1)
+            }
+            VStack {
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
+            }
+        }
+    }
+    
+    func matchMarker(peg: Int) -> some View {
+        let exactCount: Int = matches.count(where: { match in match == .exact})
+        let foundCount: Int = matches.count(where: { match in match != .nomatch})
+        return Circle()
+            .fill(exactCount > peg ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2).aspectRatio(1, contentMode: .fit)
+    }
+}
+
+#Preview {
+    MatchMarkers(matches:[.exact, .inexact, .nomatch])
+}
