0c9d61ec3f79d2adba60672b22ac88f54b1ca0e6
Benjamin <richienagle@gmail.com>
2026-07-16 11:13:31 -0700

    extracted Code struct to new file for clean up

---
+++ CodeBreaker/Code.swift
@@ -0,0 +1,69 @@
+import SwiftUI
+
+struct Code: CustomStringConvertible {
+    var kind: Kind
+    var pegs: [Peg]
+    
+    static let missingPeg: Peg = .missing
+    
+    enum Kind: Equatable {
+        case master
+        case guess
+        case attempt([Match])
+        case unknown
+    }
+    
+    init(kind: Kind, pegCount: Int) {
+        self.kind = kind
+        self.pegs = Array(repeating: Code.missingPeg, count: pegCount)
+    }
+    
+    mutating func randomize(from pegChoices: [Peg]) {
+        for index in pegs.indices {
+            pegs[index] = pegChoices.randomElement() ?? Code.missingPeg
+        }
+    }
+    
+    var matches: [Match]? {
+        switch kind {
+        case .attempt(let matches): return matches
+        default : return nil
+        }
+    }
+    
+    func match(against otherCode: Code) -> [Match] {
+        var pegsToMatch = otherCode.pegs
+        
+        let backwardsExactMatches = pegs.indices.reversed().map {index in
+            if pegsToMatch.count > index, pegsToMatch[index] == pegs[index] {
+                pegsToMatch.remove(at: index)
+                return Match.exact
+            } else {
+                return .nomatch
+            }
+        }
+        let exactMatches = Array(backwardsExactMatches.reversed())
+        return pegs.indices.map { index in
+            if exactMatches[index] != .exact, let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
+                pegsToMatch.remove(at: matchIndex)
+                return .inexact
+            } else {
+                return exactMatches[index]
+            }
+            
+        }
+    }
+    
+    var description: String {
+        let names = pegs.map { peg -> String in
+            switch peg {
+            case .blue: return "blue"
+            case .red: return "red"
+            case .green: return "green"
+            case .yellow: return "yellow"
+            default: return "missing"
+            }
+        }
+        return "Code(\(names.joined(separator: ", ")))"
+    }
+}
