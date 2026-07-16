b076f42c2aa69f833699650e0e71ac150bc18a6a
Benjamin <richienagle@gmail.com>
2026-07-16 11:28:56 -0700

    added PegView into separate file, made pegshape modifyable dynamically

---
+++ CodeBreaker/PegView.swift
@@ -0,0 +1,18 @@
+//
+//  PegView.swift
+//  CodeBreaker
+//
+//  Created by benjamin on 7/16/26.
+//
+
+import SwiftUI
+
+struct PegView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PegView()
+}
